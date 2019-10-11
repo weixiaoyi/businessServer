639807dd9ef865880e8ffbcd37b51e7edf0633bb
weixiaoyi <2278095567@qq.com>
2019-10-11 20:16:22 +0800

    feat: pm2 配置文件区分数据库local,dev,prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,24 +7,23 @@ module.exports = {
       autorestart: true,
       watch: true,
       max_memory_restart: "1G",
-      env_development: {
+      env_local: {
         PORT: 7001,
         NODE_ENV: "development",
         MONGODB_URL: "mongodb://localhost:27017/admin",
-        //  MONGODB_URL: "mongodb://admin:weixiaoyao886@47.104.71.141:27017/admin",
         MONGODB_SECRET: "weixiaoyi"
       },
-      env_predevelopment: {
+      env_development: {
         PORT: 7001,
-        NODE_ENV: "predevelopment",
-        MONGODB_URL: "mongodb://admin:weixiaoyao886@47.104.71.141:27017/admin",
+        NODE_ENV: "development",
+        MONGODB_URL: "mongodb://dev:weixiaoyao886@47.104.71.141:27017/dev",
         MONGODB_SECRET: "weixiaoyi"
       },
       env_production: {
         PORT: 7001,
         NODE_ENV: "production",
         MONGODB_SECRET: "weixiaoyi",
-        MONGODB_URL: "mongodb://admin:weixiaoyao886@47.104.71.141:27017/admin"
+        MONGODB_URL: "mongodb://prod:weixiaoyao886@47.104.71.141:27017/prod"
       }
     }
   ]
