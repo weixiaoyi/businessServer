79c4f1a06bfad3be55609aa12850080b47106365
weixiaoyi <2278095567@qq.com>
2019-10-15 21:12:57 +0800

    feat: analysis模块记录数据

--- controllers/analysis/index.js
+++ controllers/analysis/index.js
@@ -1,4 +1,6 @@
+import requestIp from "request-ip";
 import { Router } from "../../components";
+import { Analysis } from "../../models";
 
 class AnalysisController extends Router {
   constructor(props) {
@@ -7,10 +9,37 @@ class AnalysisController extends Router {
   }
 
   init = () => {
-    this.router.post("/recordInfo", this.recordInfo);
+    this.router.post("/recordAnalysis", this.recordAnalysis);
   };
 
-  recordInfo = (req, res) => {};
+  recordAnalysis = (req, res) => {
+    const ip = requestIp.getClientIp(req);
+    const {
+      domain,
+      userAgent,
+      userInfo,
+      dataType,
+      createTime,
+      stayTime,
+      records
+    } = req.body;
+    let submit = {
+      domain,
+      userAgent,
+      userInfo,
+      dataType,
+      createTime,
+      ip
+    };
+    if (dataType === "error") {
+      submit.stayTime = stayTime;
+      submit.records = records;
+    }
+    const analysis = new Analysis(submit);
+    const result = analysis.save().catch(this.handleError);
+    if (this.isError(result) || this.isNull(result)) return this.fail(res);
+    return this.success(res);
+  };
 }
 
 export default new AnalysisController().router;
